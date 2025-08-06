befe8de805847aa6b66fc12eab5bb764e107645a
TUmaMaw2 <145398958+TUmaMaw2@users.noreply.github.com>
2025-08-05 18:17:53 -0600

    Add files via upload

--- connection.py
+++ connection.py
@@ -2,6 +2,13 @@ import pymysql
 import os
 
 def get_connection():
+    print("==== DEBUG CONEXIÓN ====")
+    print("DB_HOST:", os.environ.get("DB_HOST"))
+    print("DB_USER:", os.environ.get("DB_USER"))
+    print("DB_PASSWORD:", os.environ.get("DB_PASSWORD"))
+    print("DB_NAME:", os.environ.get("DB_NAME"))
+    print("DB_PORT:", os.environ.get("DB_PORT"))
+
     return pymysql.connect(
         host=os.environ.get("DB_HOST"),
         user=os.environ.get("DB_USER"),
